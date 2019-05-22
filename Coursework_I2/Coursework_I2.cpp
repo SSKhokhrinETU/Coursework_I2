095f000b5faa5a119c906367fb32c958c543e1d4
Станислав Хохрин <sskhokhrin@stud.eltech.ru>
2019-05-22 14:52:00 +0300

    New code

--- Coursework_I2/Coursework_I2.cpp
+++ Coursework_I2/Coursework_I2.cpp
@@ -1,191 +1,296 @@
 ﻿#include "pch.h"
 #include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <stdio.h>
 #include <string>
-#include <vector>
-#include <clocale>
+#include <windows.h>
+
 using namespace std;
 
-class Zipper {
+const short SPEC_KEY = 224;
+const short UP_ARROW_KEY = 72;
+const short DOWN_ARROW_KEY = 80;
+const short LEFT_ARROW_KEY = 75;
+const short RIGHT_ARROW_KEY = 77;
+const short ENTER_KEY = 13;
+const short ESC_KEY = 27;
+const short SPACE_KEY = 32;
+const short BACKSPACE_KEY = 8;
+const short NUM_0_KEY = 48;
+const short NUM_1_KEY = 49;
+const short NUM_2_KEY = 50;
+const short NUM_3_KEY = 51;
+const short NUM_4_KEY = 52;
+const short NUM_5_KEY = 53;
+const short NUM_6_KEY = 54;
+const short NUM_7_KEY = 55;
+const short NUM_8_KEY = 56;
+const short NUM_9_KEY = 57;
+const short BLACK = 0;
+const short BLUE = 1;
+const short LIGHT_GRAY = 7;
+const short WHITE = 15;
 
-private:
-	vector<string> files;
-	string path;
-	string real_bin_file;
-public:
-	Zipper(vector<string> &vec, string p)
-	{
-		if (vec.size() > 0) files.assign(vec.begin(), vec.end());
-		path = p + "\\";
-		real_bin_file = path + "binary.zipper";
-	}
-	void getInfo();
-	void InCompress();
-	void OutCompress(string binary);
+using namespace std;
 
-	static string get_file_name(string fn) { return fn.substr(fn.find_last_of("\\") + 1, fn.size()); }
+struct ObjList
+{
+	int index;
+	WCHAR objName[MAX_PATH];
+	ObjList* next;
 };
 
-
-int digs(double w)
+void setcur(int x, int y)
 {
-	int yield = 0;
-	while (w > 10) { yield++; w /= 10; }
-	return yield + 1;
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+};
+
+void hidecursor()
+{
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	info.bVisible = FALSE;
+	SetConsoleCursorInfo(consoleHandle, &info);
 }
 
-void Zipper::getInfo()
+short objCounter(ObjList List)
 {
-	char byte[1];
-
-	basic_string<char> s_info = "";
-	remove((this->path + "info.txt").c_str());
-	FILE *info = fopen((this->path + "info.txt").c_str(), "a+");
-	int bytes_size = 0;
-	for (vector<string>::iterator itr = this->files.begin(); itr != this->files.end(); ++itr)
+	ObjList *p;
+	short counter = 0;
+	p = &List;
+	while (p != NULL)
 	{
-		FILE *f = fopen((*itr).c_str(), "rb");
-		if (!f) break;
-
-		fseek(f, 0, SEEK_END);
-		int size = ftell(f);
-		string name = Zipper::get_file_name(*itr);
-		char *m_size = new char[digs(size)];
-		_itoa(size, m_size, 10);
-		fclose(f);
-
-		bytes_size += digs(size);
-		bytes_size += strlen(name.c_str());
-
-		s_info.append(m_size);
-		s_info.append("||");
-		s_info.append(name);
-		s_info.append("||");
-
-		delete[] m_size;
-
+		p = p->next;
+		counter++;
 	}
-	bytes_size = s_info.size() + 2;
-	char *b_buff = new char[digs(bytes_size)];
-	_itoa(bytes_size, b_buff, 10);
-	if (digs(bytes_size) < 5) fputs(string(5 - digs(bytes_size), '0').c_str(), info);
-	fputs(b_buff, info);
-	fputs("||", info);
-	fputs(s_info.c_str(), info);
-
-	fclose(info);
+	counter-=2;
+	return counter;
 }
 
-void Zipper::InCompress()
+short int menu(short int num1, short int num2, short int arrow, short counter)
 {
-	char byte[1];
-
-	getInfo();
-	FILE *f, *main = fopen((this->real_bin_file).c_str(), "wb");
-	FILE *info = fopen((this->path + "info.txt").c_str(), "rb");
-	while (!feof(info))
+	if (num1 == SPEC_KEY)
 	{
-		if (fread(byte, 1, 1, info) == 1) fwrite(byte, 1, 1, main);
-	}
-
-	fclose(info);
-	remove((this->path + "info.txt").c_str());
-
-	for (vector<string>::iterator itr = this->files.begin(); itr != this->files.end(); ++itr)
-	{
-		f = fopen((*itr).c_str(), "rb");
-		if (!f) { cout << *itr << " не найден!" << endl; break; }
-		while (!feof(f))
+		if (num2 == UP_ARROW_KEY)
 		{
-			if (fread(byte, 1, 1, f) == 1) fwrite(byte, 1, 1, main);
+			if (arrow > 0)
+				arrow--;
 		}
-		cout << *itr << " добавлен в архив '" << this->real_bin_file << "'." << endl;
-		fclose(f);
+		else if ((num2 == RIGHT_ARROW_KEY) && (counter > 58))
+		{
+			if (counter < arrow + 58)
+				arrow = counter;
+			arrow += 58;
+		}
+		else if ((num2 == LEFT_ARROW_KEY) && (arrow > 58))
+		{
+			arrow -= 58;
+		} else if (num2 == DOWN_ARROW_KEY)
+		{
+			if (arrow < counter)
+				arrow++;
+		}
+		else
+			return arrow;
 	}
-	fclose(main);
+	return arrow;
 }
 
-
-void Zipper::OutCompress(string binary)
+void printScreen(ObjList obj, char disk, char dir, short ptr)
 {
-	FILE *bin = fopen(binary.c_str(), "rb");
-	char info_block_size[5];
-	fread(info_block_size, 1, 5, bin);
-	int _sz = atoi(info_block_size);
-
-	char *info_block = new char[_sz];
-	fread(info_block, 1, _sz, bin);
-
-	vector<string> tokens;
-	char *tok = strtok(info_block, "||");
-	int toks = 0;
-	while (tok)
+	//system("cls");
+	
+	setcur(0, 0);
+	hidecursor();
+	ObjList* p = &obj;
+	string nameDisk, nameDir, path;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	nameDisk = disk;
+	nameDir = dir;
+	path = nameDisk + ":" + "\\" + nameDir;
 	{
-		if (strlen(tok) == 0) break;
-		tokens.push_back(tok);
-		tok = strtok(NULL, "||");
-		toks++;
-	}
-	if (toks % 2 == 1) toks--;
-	int files = toks / 2;
-
-	char byte[1];
-
-	for (int i = 0; i < files; i++)
-	{
-		const char* size = tokens[i * 2].c_str();
-		const char* name = tokens[i * 2 + 1].c_str();
-		char full_path[255];
-		strcpy(full_path, this->path.c_str());
-		strcat(full_path, name);
-		int _sz = atoi(size);
-		cout << "--  '" << name << "' извлечен в '" << this->path << "' ." << endl;
-		FILE *curr = fopen(full_path, "wb");
-		for (int r = 1; r <= _sz; r++)
+		SetConsoleTextAttribute(hConsole, (WORD)((/*фон*/BLACK << 4) |/*текст*/ BLUE));
+		cout << '\xC9';
+		for (short i = 0; i < 40; i++)
+			cout << "\xCD";
+		SetConsoleTextAttribute(hConsole, (WORD)((BLUE << 4) | LIGHT_GRAY));
+		cout << path;
+		for (int i = 30 - size(path); i > 0; i--)
+			cout << " ";
+		if (size(path) > 30)
 		{
-			if (fread(byte, 1, 1, bin) == 1) fwrite(byte, 1, 1, curr);
+			setcur(67, 0);
+			cout << "..";
 		}
-		fclose(curr);
-
-		delete[] size;
-		delete[] name;
+		SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | BLUE));
+		for (short i = 0; i < 40; i++)
+			cout << "\xCD";
+		cout << '\xBB';
+		cout << '\xC9';
+		for (short i = 0; i < 40; i++)
+			cout << "\xCD";
+		SetConsoleTextAttribute(hConsole, (WORD)((BLUE << 4) | LIGHT_GRAY));
+		cout << path;
+		for (int i = 30 - size(path); i > 0; i--)
+			cout << " ";
+		if (size(path) > 30)
+		{
+			setcur(67, 0);
+			cout << "..";
+		}
+		SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | BLUE));
+		for (short i = 0; i < 40; i++)
+			cout << "\xCD";
+		cout << '\xBB';
 	}
-	fclose(bin);
-
+	SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | BLUE));
+	for (int i = 0; i < 58; i++)
+	{
+		cout << "\xBA";
+		if (p != NULL)
+		{
+			if (ptr == p->index)
+				SetConsoleTextAttribute(hConsole, (WORD)((LIGHT_GRAY << 4) | BLACK));
+			else
+				SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | WHITE));
+			setlocale(LC_ALL, "Russian");
+			wcout << p->objName;
+			wcout.clear();
+			p = p->next;
+			setlocale(LC_ALL, "C");
+		}
+		else
+			for (int i = 0; i < 110; i++)
+				cout << " ";
+		SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | BLUE));
+		cout << endl;
+	}
+	for (int i = 0; i < 58; i++)
+	{
+		setcur(111, i + 1);
+		cout << "\xBA\xBA";
+		if (p != NULL)
+		{
+			if (ptr == p->index)
+				SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | LIGHT_GRAY));
+			else
+				SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | WHITE));
+			setlocale(LC_ALL, "Russian");
+			wcout << p->objName;
+			wcout.clear();
+			setlocale(LC_ALL, "C");
+			p = p->next;
+		}
+		else
+			for (int i = 0; i < 110; i++)
+				cout << " ";
+		SetConsoleTextAttribute(hConsole, (WORD)((BLACK << 4) | BLUE));
+	}
+	for (int i = 0; i < 58; i++)
+	{
+		setcur(223, i + 1);
+		cout << "\xBA";
+	}
+	cout << "\xC8";
+	for (int i = 0; i < 110; i++)
+		cout << "\xCD";
+	cout << "\xBC\xC8";
+	for (int i = 0; i < 110; i++)
+		cout << "\xCD";
+	cout << "\xBC";
 }
 
-int main(int argv, char* argc[])
+ObjList objNames(ObjList obj, char disk, char dir)
 {
-	/*/  Suppotred args:
-	//
-	//    -pack, -unpack, -files, -path
-	//
-	/*/
+	//WIN32_FIND_DATAW wfd;
+	ObjList* p;
 
-	setlocale(LC_ALL, "Russian");
-	cout << endl << "######################## ZIPPER ########################" << endl << endl;
-	if (argv > 1)
+	
+	//HANDLE hFind;
+	WIN32_FIND_DATAW wfd; //WIN32_FIND_DATA find_data;
+	string nameDisk, nameDir, path;
+	wchar_t ansi[400];
+	nameDisk = disk;
+	nameDir = dir;
+	path = nameDisk + ":" + "\\" + nameDir + "*";
+
+	int num = MultiByteToWideChar(CP_UTF8, 0, path.c_str(), -1, ansi, 400);
+	LPCTSTR lpDir = ansi;
+	//hFind = FindFirstFile(lpDir, &find_data);
+
+
+	HANDLE const hFind = FindFirstFileW(lpDir, &wfd);
+	short i = 0;
+	p = &obj;
+	while (wfd.cFileName[i - 1] != 0)
 	{
-		vector<string> files;
-		string path = "";
-		bool flag_fs = false, flag_path = false;
-		char type[6];
-		memset(type, 0, 6);
-		for (int i = 1; i < argv; i++)
-		{
-			if (strcmp(argc[i], "-pack") == 0) { strcpy(type, "pack"); flag_fs = flag_path = false; }
-			if (strcmp(argc[i], "-unpack") == 0) { strcpy(type, "unpack"); flag_fs = flag_path = false; }
-			if (strcmp(argc[i], "-path") == 0) { flag_path = true; flag_fs = false; continue; }
-			if (strcmp(argc[i], "-files") == 0) { flag_fs = true; flag_path = false; continue; }
-
-			if (flag_path) { path.assign(argc[i]); }
-			if (flag_fs) files.push_back(string(argc[i]));
-
-		}
-		Zipper *zip = new Zipper(files, path);
-		if (strcmp(type, "pack") == 0) zip->InCompress();
-		if (strcmp(type, "unpack") == 0) zip->OutCompress(files[0]);
+		p->objName[i] = wfd.cFileName[i];
+		i++;
 	}
-	else cout << "Параметры -pack/-unpack , -files, -path обязательны!" << endl;
-	cout << endl << "########################################################" << endl << endl;
 
+	p->next= NULL;
+	//setlocale(LC_ALL, "");
+	
+
+	if (INVALID_HANDLE_VALUE != hFind)
+	{
+		p->next - new ObjList;
+		while (NULL != FindNextFileW(hFind, &wfd))
+		{
+			short i = 0;
+			//wcout << &wfd.cFileName[0] << endl;
+			while (wfd.cFileName[i-1] != 0)
+			{
+				p->objName[i] = wfd.cFileName[i];
+				i++;
+			}
+
+			p->next = new ObjList;
+			p->next->next = NULL;
+			p->next->index = p->index + 1;
+			p = p->next;
+			
+		} 
+		p->next = NULL;
+		FindClose(hFind);
+	}
+	return obj;
+}
+
+int main()
+{
+	ObjList head;
+	ObjList* p = &head;
+	short choose = 0, tempChoose = 0, arrowControl = 0;
+	head.index = 0;
+	head.next = NULL;
+	system("mode con cols=224 lines=60");
+	char disk = 'C';
+	char dir = '*';
+
+	system("color 08");
+
+	head = objNames(head, 'C', '*');
+	printScreen(head, 'C', '*', arrowControl);
+	while(choose != ESC_KEY)
+	{
+		do
+		{
+			choose = _getch();
+			if (choose == SPEC_KEY)
+				tempChoose = _getch();
+		} while ((choose != ESC_KEY) && (choose != SPEC_KEY) && (choose != ENTER_KEY));
+		/*if (choose == ENTER_KEY)
+			head = head = objNames(head, 'C' + , '*');*/
+		arrowControl = menu(choose, tempChoose, arrowControl, objCounter(head));
+		printScreen(head, 'C', '*', arrowControl);
+	}
+
+
+	return 0;
 }
